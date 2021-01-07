54b1b76a384f41597ca9ae4319256ac82bbe4e39
Pedro Henrique <riquefidel@outlook.com>
2021-01-07 08:56:02 -0300

    Project created

---
+++ Backend/src/main/java/com/devsuperior/dedeliver/DedeliverApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dedeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DedeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DedeliverApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/devsuperior/dedeliver/DedeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dedeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DedeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
